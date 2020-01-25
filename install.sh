1a82ed053408414f83621601ecaf9bca8b50dd67
cytrus <cytrus77@gmail.com>
2020-01-25 11:57:07 +0100

    installation sccript for Armbian Buster added

---
+++ install.sh
@@ -0,0 +1,12 @@
+apt update
+apt install pkg-config
+#wget http://ftp.de.debian.org/debian/pool/main/g/glib2.0/libglib2.0-dev_2.58.3-2+deb10u2_armhf.deb
+#dpkg -i libglib2*.deb
+apt install libglib2.0-dev
+apt-get -f install
+apt install python3-pip
+pip3 install wheel
+pip3 install setuptools
+pip3 install bluepy
+pip3 install btlewrap
+pip3 install paho-mqtt
